8b7b0cc986f8a75d540c5129d147c8f27b27098e
rizkimuliono <rizkimuliono@Rizkis-MacBook-Pro.local>
2019-12-23 00:02:23 +0700

    push 1

--- knn-1.php
+++ knn-1.php
@@ -6,7 +6,6 @@ use Phpml\Classification\KNearestNeighbors;
 
 // $samples = [[7,6,3], [6,6,4], [6,5,1], [1,3,2], [2,4,2], [2,2,3]];
 // $labels  = ['bad', 'bad', 'bad', 'good', 'good', 'good'];
-
 $samples = [[8,4], [4,5], [4,6], [7,7], [5,6], [6,5]];
 $labels  = ['baik', 'jelek', 'jelek', 'Baik', 'jelek', 'baik'];
 
